83874ef43ae17580abb50ac5ed7b9ccea92390c6
Masum Rana <masumrana446267@gmail.com>
2024-01-09 00:14:08 +0600

    feedback is submited successfully

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -1,29 +1,57 @@
+/**
+ * Title: 'Submiting feedback page'
+ * Description: ''
+ * Author: 'Masum Rana'
+ * Date: 08-01-2024
+ *
+ */
+
+// imports
 import { SubmitHandler, useForm } from "react-hook-form";
 import { IFeedback } from "@/Interface/feedback";
 import RootLayout from "@/Components/Layout/RootLayout";
 import { ReactElement } from "react";
 import ContactUs from "@/Components/Shared/HomeUi/ContactUs";
+import { useSubmitFeedbackApiMutation } from "@/Redux/api/feedbackApi";
+import Notification from "@/Components/Shared/Notification/Notification";
 
 const FeedBack = () => {
+  // react hook form
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<IFeedback>();
 
-  const onSubmit: SubmitHandler<IFeedback> = (data) => {
-    console.log(data);
+  //redux
+  const [setSubmitFeedback] = useSubmitFeedbackApiMutation();
+
+  //   handling submit feedback
+  const onSubmit: SubmitHandler<IFeedback> = async (data) => {
+    const res = await setSubmitFeedback(data).unwrap();
+
+    if (res?.statusCode == 200) {
+      Notification({
+        description:
+          "Successfully Feedback submited. Thanks for giving feedback.",
+        placement: "bottomRight",
+      });
+    }
   };
 
   return (
     <div className="mx-auto container mt-8 p-6 bg-white rounded-md shadow-lg">
+      {/* header title */}
       <div className="flex justify-center py-5 my-3">
         <h2 className="inline-block border-b-4 border-green-500 font-bold lg:text-5xl text-3xl my-10">
           Contact Us
         </h2>
       </div>
+      {/* main part  */}
       <div className="flex lg:flex-row flex-col">
+        {/* feedback inputs form */}
         <form className="w-full lg:w-1/2" onSubmit={handleSubmit(onSubmit)}>
+          {/* name  input  */}
           <div className="mb-4">
             <label
               htmlFor="name"
@@ -43,6 +71,7 @@ const FeedBack = () => {
               <p className="text-red-500 text-sm">{errors.name.message}</p>
             )}
           </div>
+          {/* email input  */}
           <div className="mb-4">
             <label
               htmlFor="email"
@@ -62,6 +91,7 @@ const FeedBack = () => {
               <p className="text-red-500 text-sm">{errors.email.message}</p>
             )}
           </div>
+          {/* message input */}
           <div className="mb-4">
             <label
               htmlFor="message"
@@ -81,6 +111,7 @@ const FeedBack = () => {
               <p className="text-red-500 text-sm">{errors.message.message}</p>
             )}
           </div>
+          {/* submit  button  */}
           <button
             type="submit"
             className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none"
