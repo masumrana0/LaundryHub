58a2b8f7e7c6ef04ec0ad952b05186ed59e70a2a
Masum Rana <masumrana446267@gmail.com>
2024-01-09 13:39:08 +0600

    profile page functionalites define

---
+++ src/Components/profile/ProfilePersonInfo.tsx
@@ -0,0 +1,18 @@
+const ProfilePersonInfo = () => {
+  return (
+    <div>
+      return (
+      <div>
+        <div className="flex justify-center items-center flex-col gap-1  ">
+          <h2 className="text-2xl font-bold  ">Personal Info</h2>
+          <p className="text-gray-500">
+            Info About you and your preferennnnces aross Laundry Services
+          </p>
+        </div>
+      </div>
+      );
+    </div>
+  );
+};
+
+export default ProfilePersonInfo;
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -9,6 +9,7 @@ import HomeProfile from "@/Components/profile/HomeProfile";
 import Footer from "@/Components/Shared/Footer";
 import Navbar from "@/Components/Shared/Navbar/Navbar";
 import { useState } from "react";
+import ProfilePersonInfo from "@/Components/profile/ProfilePersonInfo";
 
 const Profile = () => {
   const [profileSection, setProfileSection] = useState<number>(1);
@@ -28,30 +29,42 @@ const Profile = () => {
             Profile Home
           </button>
 
-          <Link className=" bg-green-200 rounded-e-2xl p-3 text-xl" href="/">
-            <button className="text-semibold flex justify-center items-center gap-1.5">
-              {" "}
-              <FaUserGear />
-              Personal Info
-            </button>
-          </Link>
-          <Link className=" bg-green-200 rounded-e-2xl p-3 text-xl" href="/">
-            <button className="text-semibold flex justify-center items-center gap-1.5">
-              {" "}
-              <FaClipboard />
-              Booking DashBoard
-            </button>
-          </Link>
-          <Link className=" bg-green-200 rounded-e-2xl p-3 text-xl" href="/">
-            <button className="text-semibold flex justify-center items-center gap-1.5">
-              {" "}
-              <FaLock />
-              Security
-            </button>
-          </Link>
+          <button
+            onClick={() => setProfileSection(2)}
+            className={`${
+              profileSection == 2 && "bg-green-200 "
+            } rounded-e-2xl p-3 text-xl text-semibold flex justify-start items-center gap-1.5`}
+          >
+            {" "}
+            <FaUserGear />
+            Personal Info
+          </button>
+
+          <button
+            onClick={() => setProfileSection(3)}
+            className={`${
+              profileSection == 3 && "bg-green-200"
+            }  rounded-e-2xl p-3 text-xl text-semibold flex justify-start items-center gap-1.5`}
+          >
+            {" "}
+            <FaClipboard />
+            Booking DashBoard
+          </button>
+
+          <button
+            onClick={() => setProfileSection(4)}
+            className={`${
+              profileSection == 4 && "bg-green-200"
+            } rounded-e-2xl p-3 text-xl text-semibold flex justify-start items-center gap-1.5`}
+          >
+            {" "}
+            <FaLock />
+            Security
+          </button>
         </div>
         <div className=" w-[80%]   mt-10">
           {profileSection == 1 && <HomeProfile />}
+          {profileSection == 2 && <ProfilePersonInfo />}
         </div>
       </div>
       <Footer />
