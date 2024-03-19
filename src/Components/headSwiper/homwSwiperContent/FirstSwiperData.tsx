ccf93f4b5494a16fe0f6106920aea4d11f93fe56
Masum Rana <masumrana446267@gmail.com>
2024-03-19 22:01:40 +0600

    <responsive> headSwiper firstSwiper data  mobile responsive done

--- src/Components/headSwiper/homwSwiperContent/FirstSwiperData.tsx
+++ src/Components/headSwiper/homwSwiperContent/FirstSwiperData.tsx
@@ -29,21 +29,21 @@ const FirstSwiperData = ({ isActive }: { isActive: boolean }) => {
       });
 
       gsap.to(".firstCUnderLine", {
-        duration: 1,
-        y: "50%",
+        duration: 3,
+        y: -50,
         display: "block",
         delay: 3,
       });
 
       gsap.to(".firstCBottomText", {
-        duration: 1,
+        duration: 3,
         x: -200,
         display: "block",
         delay: 3,
       });
 
       gsap.to(".firstCButton", {
-        duration: 1,
+        duration: 3,
         y: -45,
         display: "block",
         delay: 4,
@@ -76,19 +76,19 @@ const FirstSwiperData = ({ isActive }: { isActive: boolean }) => {
           <p className="firstCFirstText hidden   absolute top-[-3%] left-[10%] text-lg sm:text-xl md:2xl tracking-tighter font-thin text-white    ">
             We provide Best Cleaning Services
           </p>
-          <p className="firstCSecondText  absolute top-[6rem] left-[16rem] font-bold text-white      text-xl   ">
+          <p className="firstCSecondText hidden  absolute top-[6rem] left-[16rem] font-bold text-white      text-xl   ">
             with{" "}
             <span className="text-green-400">
               EcoDrive<span className="text-black">&trade;</span>
             </span>
           </p>
 
-          <div className="  absolute top-[15rem] left-[2.3rem]   border-b-4 border-green-400 w-20 mb-4   "></div>
+          <div className=" firstCUnderLine hidden  absolute top-[11rem] left-[2.6rem]   border-b-4 border-green-400 w-20 mb-4   "></div>
 
-          <h3 className="firstCBottomText absolute top-[25rem] left-[20rem] text-3xl text-white font-bold mb-5 hidden">
+          <h3 className=" firstCBottomText hidden  absolute top-[8.3rem] left-[15rem] text-sm text-white font-bold mb-5 ">
             Call us @ <span className="text-green-400">01644626735</span>
           </h3>
-          <button className="firstCButton absolute top-[31rem] left-[7.5rem] rounded px-10 py-2 shadow-lg font-semibold  transition-colors duration-300 bg-green-500  text-white hover:bg-green-700  hidden">
+          <button className="firstCButton hidden  absolute top-[12.5rem] left-[2.5rem] rounded px-3 py-2 shadow-lg font-semibold  transition-colors duration-300 bg-green-500  text-white hover:bg-green-700  ">
             Get Clean
           </button>
         </div>
